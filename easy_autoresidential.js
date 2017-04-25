ff77dd418683074531190ef2d14de6ca6fa95069
Jiri Hubacek <jiri.hubacek@gmail.com>
2017-04-25 13:42:04 +0200

    Add structure draft of autoresidential script

---
+++ easy_autoresidential.js
@@ -0,0 +1,36 @@
+/*
+    Easy autoresidential - create residential area around selected buildings.
+    Copyright (C) 2017  Jiri Hubacek
+    Contact: Jiri Hubacek <jiri.hubacek@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// for debug purposes
+//var con = require("josm/scriptingconsole");
+//con.clear();
+
+// general includes
+var JSAction = require("josm/ui/menu").JSAction;
+
+// Graham scan implementation
+// @see https://en.wikipedia.org/wiki/Graham_scan
+function ccw(p1, p2, p3)
+{
+    return (p2["lon"] - p1["lon"])*(p3["lat"] - p1["lat"]) - 
+        (p2["lat"] - p1["lat"])*(p3["lon"] - p1["lon"]);
+}
+
+function graham_scan(coords)
+{}
