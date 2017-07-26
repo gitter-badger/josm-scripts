bf2597b7129e24838685145059866d012dc84147
Jiri Hubacek <jiri.hubacek@gmail.com>
2017-07-26 19:44:49 +0200

    Add `click_building` script template

---
+++ click_building.js
@@ -0,0 +1,39 @@
+/*
+    Click building - create building by one click.
+    Copyright (C) 2017  Jiri Hubacek
+    Contact: Jiri Hubacek <jiri.hubacek@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// for debug purposes
+//var con = require("josm/scriptingconsole");
+//con.clear();
+
+/*// general includes
+var JSAction = require("josm/ui/menu").JSAction;
+
+// create click building
+var click_building = new JSAction({
+    name: "Easy Click Building",
+    tooltip: "Create click building",
+    onExecute: function() {
+        // init
+        var cmd = require("josm/command");
+        var active_layer = josm.layers.activeLayer;
+        var ds = active_layer.data;
+        var wb =  ds.wayBuilder;
+
+        // source code here
+        // ...
+}});*/
